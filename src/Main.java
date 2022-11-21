110e775fc2362b1d2a4fc435d784d2da0646096c
julianlombana <110684866+julianlombana@users.noreply.github.com>
2022-11-21 14:14:50 -0500

    if else

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int calificacion = 7 ;
+        boolean avisarFamilia = false;
+        if(calificacion>= 0 00 calificacion < 5){
+        system.out.println("insuficiente");
+          avisarFamilia = true;
+        }else if (calificacion < 7){
+          system.out.println("suficiente");
+        }else if (calificacion < 9){
+          system.out.println("Notable");
+        } else if (calificacion<10){
+            system.out.println("sobresaliente");
+        } else if (calificacion==10) {
+          system.out.println("matricula");
+        }else }{
+           system.out.println("calificacion no valida");
+         }
+         if(avisarFamiliar) {
+             syste.out.println("avisar a la familia");
+    }
+    }
